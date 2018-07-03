0118eea582c45dc7d52972d49a0ec9c5cbd57237
Shivam Raj <shivam8800@Viksits-MacBook-Pro.local>
2018-07-03 21:35:56 +0530

    Initialized the API

---
+++ src/server.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const Hapi = require('hapi');
+import routes from './routes'
+
+
+const server = Hapi.server({
+    port: 8080,
+    host: 'localhost'
+});
+
+
+const init = async () => {
+    server.route(routes);
+
+    await server.start();
+    console.log(`Server running at: ${server.info.uri}`);
+};
+
+process.on('unhandledRejection', (err) => {
+
+    console.log(err);
+    process.exit(1);
+});
+
+init();
